5231d719f2c7c10dc16cb5f4612cae1b5afd3d54
Tanmay Banerjee <67239350+tanmay-banerjee-design@users.noreply.github.com>
2024-06-18 14:01:55 +0530

    use client issue fixed

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,4 @@
-'use-client';
+'use client';
 import {
   UserGroupIcon,
   HomeIcon,
@@ -28,7 +28,7 @@ export default function NavLinks() {
         const LinkIcon = link.icon;
         return (
           <Link
-          href={link.href}
+            href={link.href}
             key={link.name}
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
